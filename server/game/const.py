62043b63e8635a1da49777db9dcbec7593ed2f46
lilrooness <jfrangoudes@gmail.com>
2021-07-26 22:49:17 +0000

    engine power modifies damage taken if above power threshold

---
+++ server/game/const.py
@@ -0,0 +1,11 @@
+BASE_LASER_DAMAGE = 20
+
+# above this threshold, the ship will take more damage when hit by weapons
+ENGINE_POWER_DAMAGE_THRESHOLD = 0.3
+
+# amount of extra damage per 0.1 above the damage threshold that the target ship will take
+# base_damage = 20, engine_power = 0.5, increase_rate = 1.6
+# extra_damage = base_damage * max(0, engine_power - ENGINE_POWER_DAMAGE_THRESHOLD) * increase_rate
+# (20 * (0.2 * 1.6)) = 6.4
+# final_damage = 6.4 + 20
+ENGINE_DAMAGE_INCREASE_RATE = 1.6
--- server/game/server_game.py
+++ server/game/server_game.py
@@ -1,5 +1,8 @@
+import math
+
 from common.entities.laser_shot import LaserShot
 from common.messages.ship_damage import ShipDamageMessage
+from server.game.const import BASE_LASER_DAMAGE, ENGINE_POWER_DAMAGE_THRESHOLD, ENGINE_DAMAGE_INCREASE_RATE
 from server.id import new_id
 from server.sessions.sessions import queue_message_for_broadcast
 
@@ -16,7 +19,7 @@ def resolve_laser_damage(system):
         target_ship = system.ships[shot.being_shot_ship_id]
         if not shooting_ship.dead and not target_ship.dead:
             target_ship = system.ships[shot.being_shot_ship_id]
-            apply_damage_to_ship(target_ship, shot.power)
+            _apply_damage_to_ship(target_ship, shot.power)
         else:
             shooting_ship.targeting_ship_id = None
 
@@ -31,16 +34,22 @@ def get_new_laser_shots(system, ticks):
 
             time_since_last_shot = ticks - ship.last_shot_time
             if time_since_last_shot * ship.shot_frequency >= 1.0:
-                shot = LaserShot(ship.id, ship.targeting_ship_id, 5, id_fun=new_id)
+                shot = LaserShot(
+                    ship.id,
+                    ship.targeting_ship_id,
+                    int(math.floor(BASE_LASER_DAMAGE*ship.power_allocation_guns)),
+                    id_fun=new_id
+                )
                 shots[shot.id] = shot
                 ship.last_shot_time = ticks
     return shots
 
-def apply_damage_to_ship(ship, damage):
+def _apply_damage_to_ship(ship, damage):
     death = False
-    shield_after_damage = max(ship.shield - damage, 0)
+    modified_damage = damage + get_additional_damage_from_target_multipliers(ship, damage)
+    shield_after_damage = max(ship.shield - modified_damage, 0)
     if shield_after_damage == 0:
-        damage_after_shield = max(damage - ship.shield, 0)
+        damage_after_shield = max(modified_damage - ship.shield, 0)
         ship.health = max(ship.health - damage_after_shield, 0)
         if ship.health == 0:
             death = True
@@ -48,4 +57,9 @@ def apply_damage_to_ship(ship, damage):
 
     ship.shield = shield_after_damage
 
-    queue_message_for_broadcast(ShipDamageMessage(ship.id, damage, death=death))
+    queue_message_for_broadcast(ShipDamageMessage(ship.id, modified_damage, death=death))
+
+def get_additional_damage_from_target_multipliers(ship, damage):
+    enginePowerAboveDamageThreshold = max(0, ship.power_allocation_engines - ENGINE_POWER_DAMAGE_THRESHOLD)
+
+    return enginePowerAboveDamageThreshold * ENGINE_DAMAGE_INCREASE_RATE * damage
