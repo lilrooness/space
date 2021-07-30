c94044f7be91b88258bb9cf8af7aaa3a6ee352d8
lilrooness <jfrangoudes@gmail.com>
2021-07-30 11:59:52 +0000

    remove "open" dialogue if the crate is open

--- client/render.py
+++ client/render.py
@@ -24,20 +24,20 @@ def render_static_ui(game, screen, old_state, font):
         crate_screen_space_coords = world_to_screen(game, crate.x, crate.y)
 
         if dist_to_crate <= CRATE_LOOT_RANGE:
-            banner_rect = banner(screen, crate_screen_space_coords[0], crate_screen_space_coords[1], "Open", font, top=-10)
-            button_rect = pygame.Rect(
-                banner_rect.x + banner_rect.width + 5,
-                banner_rect.y,
-                banner_rect.height,
-                banner_rect.height,
-            )
-            def _reqest_crate():
-                request_crate_content(game, crate_id)
-
-            button(screen, button_rect, callback=_reqest_crate)
-
             if crate_id in game.open_crates:
                 crate_window(game, screen, font, crate_screen_space_coords[0], crate_screen_space_coords[1], crate_id)
+            else:
+                banner_rect = banner(screen, crate_screen_space_coords[0], crate_screen_space_coords[1], "Open", font, top=-10)
+                button_rect = pygame.Rect(
+                    banner_rect.x + banner_rect.width + 5,
+                    banner_rect.y,
+                    banner_rect.height,
+                    banner_rect.height,
+                )
+                def _reqest_crate():
+                    request_crate_content(game, crate_id)
+
+                button(screen, button_rect, callback=_reqest_crate)
 
     return state
 
