b2ebf5eb8938e80fd5bf5c5787d7f149d63cdf0d
lilrooness <jfrangoudes@gmail.com>
2021-07-31 17:21:00 +0000

    can anchor banner to left, right, top, bottom

--- client/ui/components/banner.py
+++ client/ui/components/banner.py
@@ -2,10 +2,24 @@ import pygame.draw
 
 from client.const import scheme
 
-def banner(screen, x, y, text, font, padding=2, top=0, left=0):
+ANCHOR_CENTER = 1
+ANCHOR_RIGHT = 2
+ANCHOR_LEFT = 3
+
+def banner(screen, x, y, text, font, padding=2, top=0, left=0, anchor=ANCHOR_CENTER):
     width, height = font.size(text)
+
+    x = x
+
+    if anchor == ANCHOR_CENTER:
+        x = x - width/2 - padding + left
+    elif anchor == ANCHOR_LEFT:
+        x = x
+    elif anchor == ANCHOR_RIGHT:
+        x = x + width/2 - padding + left
+
     rect = pygame.Rect(
-        x - width/2 - padding + left,
+        x,
         y - height - padding + top,
         width + padding*2,
         height + padding*2,
